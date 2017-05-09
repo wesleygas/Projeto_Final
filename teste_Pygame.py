cca31f4085b3ffd494e6b221ae4ebc838d595f7c
Suneca <jhonata.souza.ismart@gmail.com>
2017-05-09 16:39:07 -0300

    movimento aparente

--- teste_Pygame.py
+++ teste_Pygame.py
@@ -20,6 +20,20 @@ def rot_center(image, angle):
 def vel_carro(rpm,marcha):
 	gear_ratios = [2.77,1.97,1.53,1,0.75]
 	return (rpm*(1/gear_ratios[marcha-1]))/100
+
+def mov_aparente(back, vel, x,xl):
+	backl = back
+	back_size = back.get_size()
+	x -= vel
+	xl -= vel
+
+	Display.blit(back,(x,0))
+	Display.blit(backl,(xl,0))
+	if x + back_size[0]  < 0:
+		x = back_size[0]
+	if xl + back_size[0]  < 0:
+		xl = back_size[0]
+	return x,xl
 #def check_quit():
 #	for event in pygame.event.get():
 #		if event.type == pygame.QUIT:
@@ -30,7 +44,6 @@ roda = pygame.image.load(r'.\Sprites\tire.png')
 roda = pygame.transform.scale(roda,(80,80))
 background = pygame.image.load('Background - EP_Final.png')
 background = pygame.transform.scale(background,(1280,720))
-background1 = background
 background_size = background.get_size()
 
 rodando = True
@@ -39,16 +52,7 @@ x_bg1 = background_size[0]
 
 
 while rodando:
-	vel = vel_carro(500,5)
-	x_bg -= vel
-	x_bg1 -= vel
-
-	Display.blit(background,(x_bg,0))
-	Display.blit(background1,(x_bg1,0))
-	if x_bg + background_size[0]  < 0:
-		x_bg = background_size[0]
-	if x_bg1 +  background_size[0]  < 0:
-		x_bg1 = background_size[0]
+	x_bg,x_bg1 = mov_aparente(background ,vel_carro(500,5), x_bg, x_bg1)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
