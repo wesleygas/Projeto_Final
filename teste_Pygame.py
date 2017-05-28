d2f3d4a0550d448f8130a5e8e7a9c464d355a14a
wesleygas <wesleygl16@gmail.com>
2017-05-28 18:13:27 -0300

    Música implementada

--- teste_Pygame.py
+++ teste_Pygame.py
@@ -57,7 +57,7 @@ def TextoT(display, linha, Loc, cor, tam):
 #Classes ----------------.------------------ Classes
 
 class player_car:
-	def __init__(self,roda,chassi):
+	def __init__(self,roda,chassi, gear_ratios, rpmmax):
 		self.roda = roda
 		self.chassi = chassi
 		self.rpm = 0
@@ -67,10 +67,10 @@ class player_car:
 		self.counter = 0
 		self.x_displacement = 0
 		self.rpmap = 1
-		self.rpmmax = 4000
+		self.rpmmax = rpmmax
+		self.gear_ratios = gear_ratios
 
 	def speeder(self):
-		self.gear_ratios = [0,0.75,1,1.5,1.9,2.77]
 		if(self.gear == 0 and self.speed > 0):
 			self.speed -= 0.1
 		elif(self.speed < 0):
@@ -102,7 +102,7 @@ class player_car:
 
 	def gear_up(self):
 		if(self.gear < 5):
-			rpm_ideal = 3500
+			rpm_ideal = 3500 
 			diferenca = abs(self.rpm - rpm_ideal)
 		
 			if not(self.gear == 0):
@@ -258,7 +258,7 @@ rpmv = pygame.image.load(r'.\Sprites\velocimetro_back_red.png')
 rpmc = pygame.image.load(r'.\Sprites\velocimetro_background.png')
 ponteiro = pygame.image.load(r'.\Sprites\velocimetro_bar.png')
 
-#Planos de fundo
+#----Planos de fundo
 street = pygame.image.load(r'.\Sprites\Background - EP_Final.png')
 desert = pygame.image.load(r'.\Sprites\background_deserto.jpg')
 background = street
@@ -269,10 +269,14 @@ menu = menu_engrenagem
 tela_engrenagem = pygame.image.load(r'.\Sprites\menus\store_background_2.png')
 plano = tela_engrenagem    
 
-#Carros
+#-----Carros
 blue_jeep = pygame.image.load(r'.\Sprites\carro_azul.png')
 black_suv = pygame.image.load(r'.\Sprites\jip_preto.png')
 carro_vermelho = pygame.image.load(r'.\Sprites\Camaro_vermelho.png')
+
+#-----Musicas
+
+pygame.mixer.music.load(r'top_gear.wav') #Central theme
 
 #-------------------------------------------------------------#
 
@@ -306,7 +310,7 @@ tela = 0
 rola = 0
 tier = 0
 
-carroP =  player_car(roda,blue_jeep)
+carroP =  player_car(roda,blue_jeep,[0,0.75,1,1.5,1.9,2.77],4000)
 carroP.rpm = 0
 carroP.gear = 0
 
@@ -316,13 +320,16 @@ xi = carroadv.pos[0]
 dis_total = dis = 38400 #Da linha até a origem 
 ticks = 0
 posicao = 414 #Da linha ao carro
-
+musica_on = False
 
 while rodando:
 	mouse = pygame.mouse.get_pos()
 	mouse1 = pygame.mouse.get_pressed()
 
 	if tela == 0:
+		if not musica_on:
+			pygame.mixer.music.play(-1)
+			musica_on = True
 		Display.blit(menu,(0,0))
 		play.tela(Display, (489, 500))
 		opçoes.tela(Display, (300, 600))
@@ -358,7 +365,8 @@ while rodando:
 				tela = 2
 
 	if tela == 1:
-
+		pygame.mixer.music.stop()
+		musica_on = False
 		if inicio_corrida != 0:
 
 			for event in pygame.event.get():
@@ -399,7 +407,7 @@ while rodando:
 						acelerando = False
 
 			carroP.gas_pedal(acelerando)
-			xi,ticks = carroadv.draw(Display,xi,vel,ticks,3)
+			xi,ticks = carroadv.draw(Display,xi,vel,ticks,2)
 			posicao = carroP.draw(Display)
 			
 			if dis < posicao:
@@ -426,7 +434,9 @@ while rodando:
 							mensagem = 0
 
 	if tela == 2:
-
+		if not musica_on:
+			pygame.mixer.music.play(-1)
+			musica_on = True
 		Display.blit(plano, (0,0))
 
 		if tier == 0:
